136faaa0ad2fc44d0be1aeffaa078ee936e5f87b
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2025-01-21 20:45:34 +0100

    Update how cancel unbonding records

--- tx_to_koinly.py
+++ tx_to_koinly.py
@@ -317,15 +317,16 @@ class KoinlyConverter:
                 record['Recipient'] = msg['validator_address']
                 record['Description'] = f'Delegated {amount} CHEQ to {msg["validator_address"]}'
             
-            # Cancel unbonding - only record fee and description
+            # Cancel unbonding
             elif msg_type == '/cosmos.staking.v1beta1.MsgCancelUnbondingDelegation':
                 amount = float(msg['amount']['amount']) / self.NCHEQ_TO_CHEQ
-                record['Label'].add('cost')
-                record['Sent Amount'] = ''
-                record['Sent Currency'] = ''
+                record['Label'].add('stake')
+                record['Sent Amount'] = amount
+                record['Sent Currency'] = 'CHEQ'
                 record['Received Amount'] = ''
                 record['Received Currency'] = ''
                 record['Sender'].add(self.address)
+                record['Recipient'] = msg['validator_address']
                 record['Description'] = f'Cancelled unbonding of {amount} CHEQ from {msg["validator_address"]}'
 
             # Staking Undelegate - record both fee and automatic reward withdrawal
